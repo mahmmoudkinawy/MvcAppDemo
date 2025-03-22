1875324e90a8ce2d6ea56f3ea3b7e46e5a3f465d
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2025-03-22 19:01:39 +0200

    d

---
+++ Views/Account/ProtectedPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcAppDemo.Views.Account
+{
+    public class ProtectedPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
